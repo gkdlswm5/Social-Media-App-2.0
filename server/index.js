af01006c1c08e6d29e1f6021a72b42c224313f2e
gkdlswm <andrewkimunr@gmail.com>
2020-11-18 21:26:27 -0800

    new mongoDB

--- server/index.js
+++ server/index.js
@@ -22,15 +22,19 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+// process.env.CONNECTION_URL ||
+const CONNECTION_URL =
+  "mongodb+srv://test:qweeasdd123@cluster0.zfw9e.mongodb.net/socialMedia?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
 }
 
+//process.env.MONGODB_URI ||
+
 mongoose
-  .connect(process.env.MONGODB_URI || CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
