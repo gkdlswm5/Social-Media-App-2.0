1559c763d194f000df8747350081bd1781b8e86c
gkdlswm <andrewkimunr@gmail.com>
2020-11-18 21:05:11 -0800

    minor changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+// require("dotenv").config({
+//   path: "./config/config.env",
+// });
+
+dotenv.config({
+  path: "./config/config.env",
+});
 
 import postRoutes from "./routes/posts.js";
 
@@ -13,12 +22,18 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://gkdlswm5:Duswns5193!@cluster0.4hswk.mongodb.net/SocialMedia?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+}
+
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI || CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
